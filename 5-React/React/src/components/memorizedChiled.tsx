b611a2759219287fa847e98e2baf6c69df7c74e0
Makrious-Ayman <maaakrious2004@gmail.com>
2025-04-27 18:59:31 +0300

    Finished Learning Next.JS

--- 5-React/React/src/components/memorizedChiled.tsx
+++
@@ -1,8 +0,0 @@
-import {memo} from 'react';
-
-const memorizedChiled = ({ name }: { name: string }) => {
-    console.log('👶 MemoizedChild rendered');
-    return <div>Child Component: {name}</div>;
-}
-
-export default memo(memorizedChiled);
--- 5-React/React/src/pages/RoutingPage/ReportAnIssue.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ReportAnIssue = () => {
-  return (
-    <div>ReportAnIssue</div>
-  )
-}
-
-export default ReportAnIssue
